3ad8f07356ad8f07814ad4243156dcca2d0fe3ca
bnh5992 <skagus159632@naver.com>
2023-02-04 17:54:10 +0900

    BeakJoon

---
+++ 그리디 알고리즘/블로그올린것/1931.py
@@ -0,0 +1,14 @@
+﻿num = int(input())
+playtime = []
+count = 0
+first = 0
+for i in range(num):
+    start, end = map(int, input().split())
+    playtime.append([end,start])
+
+playtimes = sorted(playtime)
+for i in playtimes:
+    if(i[1]>=first):
+        first = i[0]
+        count = count + 1
+print(count)
